63e8eb7c36e7b12eb7ccae328e32cd17fc1b1c04
Sprouse <connors38536@stu.powayusd.com>
2022-10-07 14:33:26 -0700

    started

--- chooseSummerCamp.java
+++ chooseSummerCamp.java
@@ -1,3 +1,36 @@
+import java.util.Scanner;
 public class chooseSummerCamp {
-    
+    public static void main (String[] args)
+    {
+        //create scanner object for user input
+        Scanner choice = new Scanner(System.in);
+
+        //sets the scene for the story
+        System.out.println("Choose Your Own Adventure: Summer Camp! \nType \"a\", \"b\" or \"c\" to choose an action and progress the story.");
+        System.out.println("\nWelcome to Camp Crystal Lake! Here, we have a large variety of activities to choose from. What would you like to start off with?\na. Go for a swim.\nb. Go hiking \nc. Head to the mess hall.");
+        String answer1 = choice.nextLine();
+
+        if (answer1.equals("a"))
+        {
+            System.out.println("swim + hear scream");
+            System.out.println("a. Grab an oar and get in a boat.\nb. Run to your cabin.");
+            String answer2 = choice.nextLine();
+            if (answer1.equals("a"))
+            {
+                System.out.println("You row a canoe out into the middle of the lake and float there for 2 hours. Finally, the police show up and you row back to shore. The police inform you that murderer came to the camp and killed a dozen campers. You are lucky to be alive.")
+            }
+            else if (answer1.equals("b"))
+            {
+                System.out.println("you run to cabin");
+            }
+        }
+        else if (answer1.equals("b"))
+        {
+            System.out.println("hike");
+        }
+        else if (answer1.equals("c"))
+        {
+            System.out.println("eat");
+        }
+    }
 }
